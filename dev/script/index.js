6943906b8ec90df4a44da5f4b63a5a7a3fa3b0fa
DESKTOP-8IQMI96\Creasy <yklorse@gmail.com>
2017-08-03 17:30:44 +0800

    initial

--- dev/script/index.js
+++ dev/script/index.js
@@ -1,13 +1,1 @@
-import Vue from 'vue';
-// import router from '../router';
-// import store from '../store';
-
-import App from '../component/App.vue';
-
-const app = new Vue({
-    // router,
-    // store,
-    components: {
-        App,
-    },
-}).$mount('#app');
+import '../style/style.scss';
